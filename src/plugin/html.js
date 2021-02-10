c21d6579e400cf8c4bd651e1e62d9f47fd154cfd
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-10 12:35:50 +0900

    Remove unused argument

--- src/plugin/html.js
+++ src/plugin/html.js
@@ -1,15 +1,14 @@
 import { ROOT_ID } from "../common";
 
 export const createContent = (js) =>
-  `export default String.raw\`${wrapByHtml(js, "")}\`;`;
+  `export default String.raw\`${wrapByHtml(js)}\`;`;
 
-const wrapByHtml = (js, css) => `
+const wrapByHtml = (js) => `
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  ${css && `<style type="text/css">${css}</style>`}
 </head>
 <body style="margin: 0 !important;padding: 0 !important;">
   <div id="${ROOT_ID}"></div>
