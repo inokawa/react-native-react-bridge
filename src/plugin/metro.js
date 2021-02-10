af7c2befb318e186b10f03fa35ed2056dfa9c9ae
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-10 20:05:45 +0900

    Fix to ignore css from assetExts

--- src/plugin/metro.js
+++ src/plugin/metro.js
@@ -3,12 +3,13 @@ import Metro from "metro";
 const babelTransformerPath = require.resolve("./transformer");
 const sourceExts = ["js", "ts", "jsx", "tsx"];
 const imageExts = ["bmp", "gif", "png", "jpg", "jpeg", "webp", "svg"];
+const assetExts = [...imageExts, "css"];
 
 export const bundle = async (filename) => {
   const config = await Metro.loadConfig();
-  config.resolver.sourceExts = [...sourceExts, ...imageExts, "css"];
+  config.resolver.sourceExts = [...sourceExts, ...assetExts];
   config.resolver.assetExts = config.resolver.assetExts.filter(
-    (ext) => !imageExts.includes(ext)
+    (ext) => !assetExts.includes(ext)
   );
   config.transformer.babelTransformerPath = babelTransformerPath;
 
