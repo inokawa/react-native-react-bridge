d9eab97d13477e1431b25cd091530d298fd8960b
inokawa <48897392+inokawa@users.noreply.github.com>
2021-06-26 15:11:22 +0900

    Fix test

---
+++ src/plugin/index.spec.js
@@ -0,0 +1,36 @@
+import * as fs from "fs";
+import * as path from "path";
+import { transform } from "./";
+
+const createPath = (filename) =>
+  path.join(__dirname, "../../fixtures", filename);
+const readFixture = (filename) => fs.readFileSync(filename, "utf-8");
+
+describe("transform", () => {
+  it("export default", async () => {
+    const filename = "app-export-default.jsx";
+    const filePath = createPath(filename);
+    const src = readFixture(filePath);
+    expect(
+      await transform({ filename: filePath, src, options: {} })
+    ).toMatchSnapshot();
+  });
+
+  it("export default with backticks", async () => {
+    const filename = "app-export-default-with-backticks.jsx";
+    const filePath = createPath(filename);
+    const src = readFixture(filePath);
+    expect(
+      await transform({ filename: filePath, src, options: {} })
+    ).toMatchSnapshot();
+  });
+
+  it("export default with md", async () => {
+    const filename = "app-export-default-with-md.jsx";
+    const filePath = createPath(filename);
+    const src = readFixture(filePath);
+    expect(
+      await transform({ filename: filePath, src, options: {} })
+    ).toMatchSnapshot();
+  });
+});
