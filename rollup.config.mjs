ab9c34077569f1c46811169ab19dff6ad11ede45
inokawa <48897392+inokawa@users.noreply.github.com>
2024-05-05 19:06:46 +0900

    Update build setting

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,15 @@ import typescript from "@rollup/plugin-typescript";
 import path from "path";
 import pkg from "./package.json" assert { type: "json" };
 
-const mainDir = path.dirname(pkg.main);
-const moduleDir = path.dirname(pkg.module);
+const external = (id) =>
+  [
+    "metro",
+    "metro-react-native-babel-transformer",
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.devDependencies || {}),
+  ].some((d) => id.startsWith(d));
 
+/** @type { import('rollup').RollupOptions[] } */
 export default [
   {
     input: "src/index.ts",
@@ -26,53 +32,58 @@ export default [
         exclude: ["src/**/*.spec.*"],
       }),
     ],
+    external,
   },
   {
     input: "src/web/index.ts",
     output: [
       {
-        file: `${mainDir}/web/index.js`,
+        file: pkg.exports["./lib/web"].default,
         format: "cjs",
       },
       {
-        file: `${moduleDir}/web/index.mjs`,
+        file: pkg.exports["./lib/web"].import,
         format: "es",
       },
     ],
     plugins: [typescript()],
+    external,
   },
   {
     input: "src/web/preact.ts",
     output: [
       {
-        file: `${mainDir}/web/preact.js`,
+        file: pkg.exports["./lib/web/preact"].default,
         format: "cjs",
       },
       {
-        file: `${moduleDir}/web/preact.mjs`,
+        file: pkg.exports["./lib/web/preact"].import,
         format: "es",
       },
     ],
     plugins: [typescript()],
+    external,
   },
   {
     input: "src/plugin/index.js",
     output: [
       {
-        file: `${mainDir}/plugin/index.js`,
+        file: pkg.exports["./lib/plugin"].default,
         format: "cjs",
         // https://github.com/inokawa/react-native-react-bridge/issues/126
         interop: "auto",
       },
     ],
+    external,
   },
   {
     input: "src/plugin/transformer.js",
     output: [
       {
-        file: `${mainDir}/plugin/transformer.js`,
+        file: `${path.dirname(pkg.main)}/plugin/transformer.js`,
         format: "cjs",
       },
     ],
+    external,
   },
 ];
