707b649b9db56346c6bd7333b50f67b76cb278ca
inokawa <48897392+inokawa@users.noreply.github.com>
2024-05-07 01:03:47 +0900

    Refactor

--- src/plugin/bundler.spec.ts
+++ src/plugin/bundler.spec.ts
@@ -1,32 +1,23 @@
 import * as path from "node:path";
-import * as vm from "node:vm";
 import { vi, describe, it, expect } from "vitest";
 import { bundle } from "./bundler";
 import { readdirSync } from "node:fs";
+import { wrapWithWebViewHTML } from "./html";
 
 vi.setConfig({ testTimeout: 30000 });
-
-const runInVmContext = (code: string): string => {
-  let evaluatedStr: string;
-  const context = vm.createContext({
-    render: (str: string) => {
-      evaluatedStr = str;
-    },
-  });
-
-  vm.runInContext(`render(${code});`, context);
-  return evaluatedStr!;
-};
 
 describe("bundle", () => {
   const fixturePath = path.join(process.cwd(), "fixtures");
   readdirSync(fixturePath).forEach((filename) => {
     if (filename.endsWith(".jsx") || filename.endsWith(".tsx")) {
       it(filename, async () => {
-        const res = await bundle(path.join(fixturePath, filename));
+        const res = await bundle(
+          path.join(fixturePath, filename),
+          wrapWithWebViewHTML
+        );
         expect(res).toMatchSnapshot();
 
-        expect(runInVmContext(res)).toMatchSnapshot();
+        expect(eval(res)).toMatchSnapshot();
       });
     }
   });
