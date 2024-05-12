e97fbc83af7b1206faa93a13314095d5719ad15f
inokawa <48897392+inokawa@users.noreply.github.com>
2024-05-12 23:10:53 +0900

    Update e2e

---
+++ fixtures/with-image-svg.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { webViewRender } from "react-native-react-bridge/lib/web";
+import url from "./assets/twitter.svg";
+
+const App = () => {
+  return (
+    <div>
+      <img src={url} />
+    </div>
+  );
+};
+
+export default webViewRender(<App />);
